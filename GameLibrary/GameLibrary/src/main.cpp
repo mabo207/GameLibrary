d5c23586d7711fcc361680ada474c963dd8098ae
mabo207 <mabo168general+github@gmail.com>
2019-11-09 18:11:18 +0900

    main関数内をゲームプログラミングに適した形式にした

--- GameLibrary/GameLibrary/src/main.cpp
+++ GameLibrary/GameLibrary/src/main.cpp
@@ -1,8 +1,66 @@
-#include "DxLib.h"
+#include<cassert>
+#include<stdexcept>
+#include"DxLib.h"
+
 
 int WINAPI WinMain(HINSTANCE,HINSTANCE,LPSTR,int){
-	DxLib_Init();	// DXライブラリ初期化処理
-	WaitKey();	// キー入力待ち
-	DxLib_End();	// DXライブラリ終了処理
-	return 0;
+	try{
+		//dxライブラリの初期化
+		//画面モードの設定(一応こんな感じ)
+		SetGraphMode(960,540,16);
+		//ウインドウサイズの変更
+		SetWindowSizeExtendRate(1.0);
+		//タイトルメニュー文字
+		SetMainWindowText("ゲームライブラリのテスト");
+		//ウインドウサイズの変更をできるようにする
+		SetWindowSizeChangeEnableFlag(TRUE);
+		//アイコンの設定
+		SetWindowIconID(101);
+		//非アクティブ状態での処理の続行のフラグ
+		SetAlwaysRunFlag(FALSE);
+		//背景色の設定
+		SetBackgroundColor(0,0,0);
+		//マウス表示設定
+		SetMouseDispFlag(TRUE);
+		//DXアーカイバの使用
+		SetUseDXArchiveFlag(TRUE);
+		
+		if(ChangeWindowMode(TRUE) != 0) {
+			throw(std::runtime_error("ChangeWindowMode(TRUE) failed."));
+		}
+		if(DxLib_Init() != 0) {
+			throw(std::runtime_error("DxLib_Init() failed."));
+		}
+		if(SetDrawScreen(DX_SCREEN_BACK) != 0) {
+			DxLib_End();
+			throw(std::runtime_error("SetDrawScreen(DX_SCREEN_BACK) failed."));
+		}
+
+		//ゲーム本体
+		while(ScreenFlip() == 0 && ProcessMessage() == 0 && ClearDrawScreen() == 0) {
+			//入力情報更新
+
+			//描画
+			clsDx();
+			DrawBox(40,40,120,120,GetColor(127,127,127),TRUE);
+
+			//情報更新
+
+
+			//遷移処理
+
+
+			//終了検出
+			
+		}
+
+		//素材の解放
+
+		//終了処理
+		DxLib_End();
+		return 0;
+	} catch(const std::exception &e){
+		assert(e.what());
+		return 1;
+	}
 }
